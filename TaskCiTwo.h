bb6fd199d7281d4d7ea437948775cb5a203bb834
Serafim1979 <korolkov.ivan@mail.ru>
2019-08-31 21:04:56 +0400

    added menu item

--- TaskCiTwo.h
+++ TaskCiTwo.h
@@ -9,5 +9,7 @@ void fraction_calc(void);
 int nod_fraction(int a, int b);
 int sum_of_digits(int n);
 int reversed_number(int n);
+int check_for_divisibility_by_three_and_nine(int n);
+int countOfDigits(int n);
 
 #endif // TASKCITWO_H_
--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    int select, num;
+    int select, num, count;
     menu();
 
     while((select = get_int()) != 27)
@@ -22,6 +22,26 @@ int main()
                     num = get_int();
                     num = sum_of_digits(num);
                     printf("The sum of the digits of the number you entered: %d", num);
+                break;
+            case 4 : printf("Enter a four-digit number: \n");
+                     num = get_int();
+                     count = countOfDigits(num);
+                     if(count == 4)
+                     {
+                         num = check_for_divisibility_by_three_and_nine(num);
+                         if(num == 1)
+                            printf("The number is divided by three or nine.\n");
+                         else
+                            printf("The number is not divisible by three or nine.\n");
+                     }
+                     else
+                     {
+                         printf("Input format error. You entered a non-four digit number.\n");
+                     }
+                break;
+            case 5 : printf("paragraph 5\n");
+                break;
+            case 6 : printf("paragraph 6\n");
                 break;
             case 7 : printf("paragraph 3\n");
                     printf("Enter an integer: \n");
